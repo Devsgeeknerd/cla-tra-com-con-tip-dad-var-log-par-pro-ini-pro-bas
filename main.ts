7b5dc192a72bb63f96de1399e0b8895e4b981e61
Paulo <vanderley.1109@gmail.com>
2024-01-22 14:28:58 -0300

    adicionando conteudo

---
+++ main.ts
@@ -0,0 +1,8 @@
+// Declaração de uma variável 'nome' do tipo string e atribuição do valor "Paulo" a ela.
+const nome: string = "Paulo";
+
+// Impressão do valor da variável 'nome' no console.
+console.log(nome);
+
+// Declaração de uma variável 'pi' do tipo number e atribuição do valor 3.14 a ela.
+const pi: number = 3.14;
